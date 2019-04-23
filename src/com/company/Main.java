e36104e6c0e0b24a667c63f9e48820c287516c69
Serhii Miroshnichenko <ragazzo@ukr.net>
2019-04-23 18:28:37 +0300

    cosmetics

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,17 +7,15 @@ public class Main {
         Theater theater = new Theater("Olympian", 8, 12);
         theater.getSeats();
 
-        if (theater.reserveSeat("H11")){
+        if (theater.reserveSeat("H11")) {
             System.out.println("Please Pay");
-        }
-        else {
+        } else {
             System.out.println("Sorry seat is taken");
         }
 
-        if (theater.reserveSeat("H11")){
+        if (theater.reserveSeat("H11")) {
             System.out.println("Please Pay");
-        }
-        else {
+        } else {
             System.out.println("Sorry seat is taken");
         }
 
