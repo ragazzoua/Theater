1012a74056b34563a64da073769989418f5b0cb2
Serhii Miroshnichenko <ragazzo@ukr.net>
2019-04-23 18:10:50 +0300

    theater in Main created

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,23 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Theater theater = new Theater("Olympian", 8, 12);
+        theater.getSeats();
+
+        if (theater.reserveSeat("H11")){
+            System.out.println("Please Pay");
+        }
+        else {
+            System.out.println("Sorry seat is taken");
+        }
+
+        if (theater.reserveSeat("H11")){
+            System.out.println("Please Pay");
+        }
+        else {
+            System.out.println("Sorry seat is taken");
+        }
+
     }
 }
