b51dcc5ea20eac463c9631991994ca57c185a1ba
Serhii Miroshnichenko <ragazzo@ukr.net>
2019-05-16 12:39:57 +0300

    Map class created

---
+++ src/com/company/MapProgram.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapProgram {
+    public static void main(String[] args) {
+        Map<String, String> languages = new HashMap<>();
+    }
+}
