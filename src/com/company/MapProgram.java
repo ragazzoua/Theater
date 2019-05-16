59d8fb6429b91b6b139cdc19c667b297c42cea5b
Serhii Miroshnichenko <ragazzo@ukr.net>
2019-05-16 14:35:34 +0300

    Map class diff method created

--- src/com/company/MapProgram.java
+++ src/com/company/MapProgram.java
@@ -6,5 +6,36 @@ import java.util.Map;
 public class MapProgram {
     public static void main(String[] args) {
         Map<String, String> languages = new HashMap<>();
+        languages.put("Java", "a compiled high level, object-oriented");
+        languages.put("Python", "an interpreteted, object-oriented");
+        languages.put("Algol", "an algoritmic languages");
+        languages.put("BASIC", "Beginers All purposes");
+        languages.put("Lisp", "Therein lies madnes");
+
+        if (languages.containsKey("Java")) {
+            System.out.println("java is already in the map");
+        } else {
+            languages.put("Java", "this course is about Java");
+        }
+
+        System.out.println("========================");
+
+//        languages.remove("Lisp");
+        if (languages.remove("Algol", "an algoritmic languages1")) {
+            System.out.println("Algol removed");
+        } else {
+            System.out.println("Algol not removed, key/value pair not found");
+        }
+        if (languages.replace("Lisp","Therein lies madnes", "a functional programing language")){
+            System.out.println("Lisp replaced");
+        }
+        else {
+            System.out.println("Lisp was not replaced");
+        }
+        //System.out.println(languages.replace("Scala", "this will not be added"));
+
+        for (String key : languages.keySet()) {
+            System.out.println(key + " : " + languages.get(key));
+        }
     }
 }
